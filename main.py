3567eb911a7b5e0b82af908ce2fc36e8dfc393f1
t0fum4n <driftka24se@gmail.com>
2023-01-06 09:47:08 -0600

    added static window size 600x300 and changed field sizes

--- main.py
+++ main.py
@@ -46,41 +46,41 @@ def run_script():
 # Consumer Key label and entry
 consumer_key_label = tk.Label(text="Consumer Key:")
 consumer_key_label.grid(row=0, column=0)
-consumer_key_entry = tk.Entry()
+consumer_key_entry = tk.Entry(width=65)
 consumer_key_entry.grid(row=0, column=1)
 
 # Consumer Secret label and entry
 consumer_secret_label = tk.Label(text="Consumer Secret:")
 consumer_secret_label.grid(row=1, column=0)
-consumer_secret_entry = tk.Entry()
+consumer_secret_entry = tk.Entry(width=65)
 consumer_secret_entry.grid(row=1, column=1)
 
 # Access Token label and entry
 access_token_label = tk.Label(text="Access Token:")
 access_token_label.grid(row=2, column=0)
-access_token_entry = tk.Entry()
+access_token_entry = tk.Entry(width=65)
 access_token_entry.grid(row=2, column=1)
 
 # Access Token Secret label and entry
 access_token_secret_label = tk.Label(text="Access Token Secret:")
 access_token_secret_label.grid(row=3, column=0)
-access_token_secret_entry = tk.Entry()
+access_token_secret_entry = tk.Entry(width=65)
 access_token_secret_entry.grid(row=3, column=1)
 
 # OpenAI Key label and entry
 openai_key_label = tk.Label(text="OpenAI Key:")
 openai_key_label.grid(row=4, column=0)
-openai_key_entry = tk.Entry()
+openai_key_entry = tk.Entry(width=65)
 openai_key_entry.grid(row=4, column=1)
 
 # Prompt label and entry
 prompt_label = tk.Label(text="Prompt:")
 prompt_label.grid(row=5, column=0)
-prompt_entry = tk.Entry()
+prompt_entry = tk.Entry(width=65)
 prompt_entry.grid(row=5, column=1)
 
 # "Run" button
-run_button = tk.Button(text="Run", command=run_script)
+run_button = tk.Button(text="Run", width=20, command=run_script)
 run_button.grid(row=6, column=0)
 
 # Success label
@@ -92,7 +92,7 @@ response_label = tk.Label(text="")
 response_label.grid(row=8, column=0)
 
 # Adjust size
-window.geometry("500x400")
+window.geometry("600x300")
 
 
 # Run the Tkinter event loop
